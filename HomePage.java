8cfe2193b34f65fb728aa3ef5563c2dc24200f3b
Muhammad Ramadhan <60300872@udst.edu.qa>
2026-04-02 09:15:12 +0300

    Created the reservation page, might need some changes later

---
+++ HomePage.java
@@ -0,0 +1,37 @@
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HomePage {
+
+    private Stage stage;
+
+    public HomePage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void initializeComponents() {
+
+        Button reservationBtn = new Button("Reservations");
+        Button orderBtn = new Button("Orders");
+        Button menuBtn = new Button("Menu");
+        Button inventoryBtn = new Button("Inventory");
+        Button paymentBtn = new Button("Payments");
+
+        VBox layout = new VBox(10,
+                reservationBtn,
+                orderBtn,
+                menuBtn,
+                inventoryBtn,
+                paymentBtn
+        );
+
+        layout.setPadding(new Insets(20));
+
+        stage.setScene(new Scene(layout, 400, 300));
+        stage.setTitle("Restaurant System");
+        stage.show();
+    }
+}
