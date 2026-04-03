d4775c95c42b728334c71528e3d24c75ce8651e6
Omar ahmad <60303791@udst.edu.qa>
2026-04-04 00:39:41 +0300

    Add RBAC role-based button visibility and logout functionality

--- HomePage.java
+++ HomePage.java
@@ -7,58 +7,72 @@ import javafx.stage.Stage;
 public class HomePage {
 
     private Stage stage;
+    private User user;
 
-    public HomePage(Stage stage) {
+    public HomePage(Stage stage, User user) {
         this.stage = stage;
+        this.user = user;
     }
 
     public void initializeComponents() {
 
         Button reservationBtn = new Button("Reservations");
         reservationBtn.setOnAction(e -> {
-            Reservation res = new Reservation(stage);
+            Reservation res = new Reservation(stage, user);
             res.initializeComponents();
         });
         Button viewResBtn = new Button("View Reservations");
         viewResBtn.setOnAction(e -> {
-            ViewReservation view = new ViewReservation(stage);
+            ViewReservation view = new ViewReservation(stage , user);
             view.initializeComponents();
         });
         Button orderBtn = new Button("Orders");
         orderBtn.setOnAction(e -> {
-            CreateOrder order = new CreateOrder(stage);
+            CreateOrder order = new CreateOrder(stage, user);
             order.initializeComponents();
         });
         Button viewOrderBtn = new Button("View Orders");
         viewOrderBtn.setOnAction(e -> {
-            ViewOrders view = new ViewOrders(stage);
+            ViewOrders view = new ViewOrders(stage, user);
             view.initializeComponents();
         });
-        Button menuBtn = new Button("Menu");
-        Button addMenuBtn = new Button("Add Menu Item");
-        addMenuBtn.setOnAction(e -> {
-            AddMenu add = new AddMenu(stage);
+        Button ManageMenuBtn = new Button("Manage Menu");
+        ManageMenuBtn.setOnAction(e -> {
+            AddMenu add = new AddMenu(stage, user);
             add.initializeComponents();
         });
         Button viewMenuBtn = new Button("View Menu");
         viewMenuBtn.setOnAction(e -> {
-            ViewMenu view = new ViewMenu(stage);
+            ViewMenu view = new ViewMenu(stage, user);
             view.initializeComponents();
         });
         Button inventoryBtn = new Button("Inventory");
         Button paymentBtn = new Button("Payments");
+
+        Button logoutBtn = new Button("Logout");
+        logoutBtn.setOnAction(e -> {
+            UserLogin login = new UserLogin(stage);
+            login.initializeComponents();
+        });
 
         VBox layout = new VBox(10,
                 reservationBtn,
                 viewResBtn,
                 orderBtn,
                 viewOrderBtn,
-                menuBtn,
-                addMenuBtn,
+                ManageMenuBtn,
                 viewMenuBtn,
                 inventoryBtn,
-                paymentBtn
+                paymentBtn,
+                logoutBtn
         );
+
+        if (AuthorizationService.isAdmin(user) ||
+                AuthorizationService.isManager(user)) {
+        } else {
+            ManageMenuBtn.setVisible(false);
+            inventoryBtn.setVisible(false);
+        }
 
         layout.setPadding(new Insets(20));
 
